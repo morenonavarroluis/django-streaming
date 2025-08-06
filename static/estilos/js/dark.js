f8e889d2ea7b325ca6381fe76bfeb3a2cf920b88
Luis Navarro <lm395746@gmail.com>
2025-08-06 16:58:36 -0400

    cambios en el dark mode

--- static/estilos/js/dark.js
+++ static/estilos/js/dark.js
@@ -1,20 +1,24 @@
 
       document.addEventListener('DOMContentLoaded', () => {
+            const html = document;
             const toggleBtn = document.getElementById('themeToggle');
             const themeIcon = document.getElementById('themeIcon');
             const body = document.body;
+            const conta = document.querySelector('.conta');
 
             // Verificar preferencia guardada
             const savedTheme = localStorage.getItem('theme');
             if (savedTheme === 'dark') {
-                body.classList.add('dark-mode');
+                //body.classList.add('dark-mode');
+                html.data.add('dark-mode');
+                conta.classList.add('dark-mode');
                 themeIcon.className = 'fas fa-sun';
             }
 
             // Manejar clic del botón
             toggleBtn.addEventListener('click', () => {
                 body.classList.toggle('dark-mode');
-                
+                conta.classList.toggle('dark-mode');
                 if (body.classList.contains('dark-mode')) {
                     themeIcon.className = 'fas fa-sun';
                     localStorage.setItem('theme', 'dark');
